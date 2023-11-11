278753188fda28e0a774214002b98f675e81c4b8
Jon R. Helms <helmsjr@skipfire.com>
2023-11-11 10:35:34 -0600

    Update Setup-2-RaspberryPi.sh

--- Setup-2-RaspberryPi.sh
+++ Setup-2-RaspberryPi.sh
@@ -1,6 +1,7 @@
 echo "Install Log2ram"
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | sudo tee /etc/apt/sources.list.d/azlux.list
+sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
 sudo apt update -y
 sudo apt install log2ram
 sudo sed -i "s/SIZE=40M/SIZE=250M/g" /etc/log2ram.conf
