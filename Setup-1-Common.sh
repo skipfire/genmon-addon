6f88805c48e12c1d0a376a13e6f59cfca841917d
Jon R. Helms <helmsjr@skipfire.com>
2023-11-11 11:43:51 -0600

    Update Setup-1-Common.sh

--- Setup-1-Common.sh
+++ Setup-1-Common.sh
@@ -2,7 +2,7 @@ echo "Update & upgrade apt"
 sudo apt update -y && sudo apt upgrade -y
 
 echo "Install some common tools"
-sudo apt install -y locales-all git i2c-tools cron git nano
+sudo apt install -y locales-all git i2c-tools cron nano
 
 echo "Setup locale"
 sudo sed -i "s/en_GB/en_US/g" /etc/default/locale
