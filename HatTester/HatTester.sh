57446c0f3f4248bcb521239bb2c2a2afa6355eb8
Jon R. Helms <helmsjr@skipfire.com>
2023-12-02 10:11:53 -0600

    Update HatTester.sh

--- HatTester/HatTester.sh
+++ HatTester/HatTester.sh
@@ -1,7 +1,9 @@
 ~/genmon/startgenmon.sh stop
 sudo systemctl stop CaptivePortal
+sudo systemctl stop PintSizeFanManager
 source ./venv/bin/activate
 python HatTester.py
 deactivate
 sudo systemctl start CaptivePortal
+sudo systemctl start PintSizeFanManager
 ~/genmon/startgenmon.sh start
