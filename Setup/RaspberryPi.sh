45ad900a68b9fbeb71a5a94695bccb0b159a3381
Jon R. Helms <helmsjr@skipfire.com>
2023-12-03 11:34:09 -0600

    Split log2ram setup

--- Setup/RaspberryPi.sh
+++ Setup/RaspberryPi.sh
@@ -4,13 +4,7 @@ cd ~/genmon-addon/
 git config --global --add safe.directory '*'
 
 echo "Install Log2ram"
-echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ bookworm main" | sudo tee /etc/apt/sources.list.d/azlux.list
-sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
-sudo apt update -y
-sudo apt install log2ram
-sudo sed -i "s/SIZE=40M/SIZE=250M/g" /etc/log2ram.conf
-sudo sed -i "s/LOG_DISK_SIZE=100M/LOG_DISK_SIZE=300M/g" /etc/log2ram.conf
-# After reboot, Verify with `sudo df -h`, you should see a log2ram line pointing to /var/log. `sudo mount` can also be used and a line should be seen with `log2ram on /var/log type tmpfs...`
+/bin/bash ./Log2Ram.sh
 
 echo "Install PintSizeFanManager"
 /bin/bash ~/genmon-addon/FanManager/Setup.sh
