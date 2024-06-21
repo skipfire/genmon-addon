59a64c237709f4e906165ac1ca4df353bbdfed5a
Jon R. Helms <helmsjr@skipfire.com>
2024-06-21 14:26:49 -0500

    Update HatTesterLegacy.sh

--- HatTester/HatTesterLegacy.sh
+++ HatTester/HatTesterLegacy.sh
@@ -1,5 +1,3 @@
 ~/genmon/startgenmon.sh stop
-sudo systemctl stop CaptivePortal
 python /home/genmonpi/genmon-addon/HatTester/HatTester.py
-sudo systemctl start CaptivePortal
 ~/genmon/startgenmon.sh start
