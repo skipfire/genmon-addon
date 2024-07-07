335812633e244c0a17912568a2d6f2309c655417
Jon R. Helms <helmsjr@skipfire.com>
2024-07-07 14:24:29 -0500

    Update HatTester.sh

--- HatTester/HatTester.sh
+++ HatTester/HatTester.sh
@@ -1,7 +1,9 @@
 ~/genmon/startgenmon.sh stop
 sudo systemctl stop LinuxConfigHelper
+print("LinuxConfigHelper stop")
 source ./venv/bin/activate
 python /home/genmonpi/genmon-addon/HatTester/HatTester.py
 deactivate
+print("LinuxConfigHelper start")
 sudo systemctl start LinuxConfigHelper
 ~/genmon/startgenmon.sh start
